1245f15c72d5f401e8e7d0b7f87b93871ea80245
Ahmet-software <f241230015@ktun.edu.tr>
2026-08-14 09:42:10 +0300

    Delete Desktop/projectify_funding_radar2 directory
    silindi

--- Desktop/projectify_funding_radar2/projectify_funding_radar2/reports/__init__.py
+++
@@ -1,5 +0,0 @@
-from .excel_report import export_results_to_excel
-from .pdf_report import export_results_to_pdf
-from .summary_report import summarize
-
-__all__ = ["export_results_to_excel", "export_results_to_pdf", "summarize"]
--- Desktop/projectify_funding_radar2/projectify_funding_radar2/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import (academic_form, company_form, entrepreneur_form, home_page,
-               project_summary_form, reports_page, results_dashboard,
-               scan_dashboard, source_manager, streamlit_app)
-
-__all__ = [
-    "home_page", "company_form", "academic_form", "entrepreneur_form",
-    "project_summary_form", "source_manager", "scan_dashboard",
-    "results_dashboard", "reports_page", "streamlit_app",
-]
